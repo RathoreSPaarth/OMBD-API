b3433c6f93c2872955efa80780d49c4a23e957b8
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-07-10 19:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-// const axios = require('axios')
 let btn = document.getElementById('btn')
 let movieName = document.getElementById('movie-name')
-// let movie = movieName.value
 btn.addEventListener('click',fetchMovie)
 
 function fetchMovie(){
     let row = document.getElementById('details')
-    let post = document.getElementById('poster-image')
+    
     while(row.firstChild){
         row.removeChild(row.firstChild)
     }
-    while(post.firstChild){
-        post.removeChild(post.firstChild)
-    }
+
     axios.get('http://www.omdbapi.com/?apikey=b186daa5&s='+ movieName.value)
     .then((res)=>{
         console.log(res.data.Search)
@@ -24,19 +20,38 @@ function fetchMovie(){
            let y =  document.createElement('div')
            let poster = document.createElement('img')
 
+           //p tags for movie info
+           let title = document.createElement('p')
+           let type = document.createElement('p')
+           let year = document.createElement('p')
+           let imdb = document.createElement('p')
+
            poster.src = list[i].Poster
+           poster.setAttribute('alt','OOPS! IMAGE NOT FOUND!')
 
-           x.setAttribute('class','col-lg-4')
-           y.setAttribute('class','col-lg-4')
-           x.setAttribute('style','border:solid 1px black;')
+           title.innerHTML = "TITLE: " + list[i].Title
+           type.innerHTML = "TYPE: " +  list[i].Type
+           year.innerHTML = "YEAR: " + list[i].Year
+           imdb.innerHTML = "FIND MORE AT: " + "https://www.imdb.com/title/"+list[i].imdbID
 
-           x.innerHTML = "This movie is "+list[i].Title
+
+           x.setAttribute('class','col-lg-4 col-12')
+           x.setAttribute('style',"border: none; border-radius:5%; background-color: #e4e3e3; color: black; ")
+           y.setAttribute('class','col-lg-4 col-12')
+           y.setAttribute('style',"margin: 5%;")
 
            let details = document.getElementById('details')
            let image = document.getElementById('poster-image')
 
+          
+          
+
            y.appendChild(poster)
-           image.appendChild(y)
+           x.appendChild(y)
+           x.appendChild(title)
+           x.appendChild(type)
+           x.appendChild(year)
+           x.appendChild(imdb)
            details.appendChild(x)
 
         }
